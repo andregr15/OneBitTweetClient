49e996862fb8c5f78ba61d901e0dc984f0106a05
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-12-18 17:22:42 -0200

    Adding TrendingTopicsContainer, TrendingTopics and Hashtag

---
+++ src/components/TrendingTopics/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import Hashtag from '../Hashtag';
+
+const CardContentTrending = styled.div`
+  padding-top: 10px !important;
+  padding-bottom: 10px !important;
+  border-radius: 0 0 0px 0px !important;
+`;
+
+const CardTitle = styled.p`
+  font-size: 18px;
+  color: white;
+`;
+
+const TrendingTopics = (props) => (
+  <div className="card">
+    <CardContentTrending className="card-content grey darken-2">
+      <CardTitle className="white-text">
+        Trending Topics
+      </CardTitle>
+    </CardContentTrending>
+    <div className="card-content">
+      {
+        props.hashtags.map(
+          (hashtag, i) => <Hashtag title={hashtag[0]} count={hashtag[1]} key={i} />
+        )
+      }
+    </div>
+  </div>
+);
+
+export default TrendingTopics;
