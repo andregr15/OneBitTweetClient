739b07e0eef1ab337e66bee669cdcd28856148ab
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-12-19 16:24:24 -0200

    Adding redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Reducers = combineReducers({
+  trendings: () => ({
+    hashtags: [['#OneBitCode', '10k'], ['#RubyOnRails', '10k']]
+  })
+});
+
+const Store = createStore(Reducers);
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
+
 registerServiceWorker();
