233d929ffcb8076de7e58bf65b1db8f6853622f2
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-12-19 14:36:43 -0200

    Getting trending topics from the api

--- src/containers/TrendingTopicsContainer/index.js
+++ src/containers/TrendingTopicsContainer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TrendingTopics from '../../components/TrendingTopics';
+import axios from 'axios';
 
 class TrendingTopicsContainer extends Component {
   constructor(props) {
@@ -11,9 +12,21 @@ class TrendingTopicsContainer extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      hashtags: [['#OneBitCode', '10k'], ['#RubyOnRails', '10k']]
-    })
+    axios.get(process.env.REACT_APP_API + '/trending')
+      .then(
+        (response) => {
+          console.log(response.data.hashtags);
+          this.setState(
+            () => {
+              return {
+                hashtags: response.data.hashtags
+              }
+            }
+          )
+        }
+      ).catch(function (error){
+        console.log(error);
+      });
   }
 
   render() {
