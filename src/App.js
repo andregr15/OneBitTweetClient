7d73c3918fc29e6c2a3f99a8a297dc31e3883db3
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-12-26 11:31:03 -0200

    Adding SignIn component and container

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Link to="/wellcome">Home</Link> <br/>
-        <Link to="/user/1">User 1 profile</Link>
         <Switch>
           <Route exact path="/" component={HomePageContainer} /> 
           <Route exact path="/user/:id" component={ProfilePageContainer} />
