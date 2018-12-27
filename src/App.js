caf498d1f50bf8680d6285bdcf316af354191f25
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-12-27 10:42:19 -0200

    Adding Timeline component and container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import HomePageContainer from './containers/HomePageContainer'
 import ProfilePageContainer from './containers/ProfilePageContainer'
+import TimelineContainer from './containers/TimelineContainer';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePageContainer} /> 
           <Route exact path="/user/:id" component={ProfilePageContainer} />
+          <Route exact path='/timeline' component={TimelineContainer} />
         </Switch>
       </div>
     );
---
+++ src/containers/TimelineContainer/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Timeline from '../../components/Timeline';
+
+class TimelineContainer extends Component {
+  render() {
+    return (
+      <Timeline />
+    );
+  }
+};
+
+export default TimelineContainer;
